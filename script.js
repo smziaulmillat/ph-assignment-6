8437e016b1dc1672d3d974f5f76c3045f2cd5a3b
smziaulmillat <asique1104@gmail.com>
2025-09-08 06:42:24 +0600

    added categories button func. com.

--- script.js
+++ script.js
@@ -65,9 +65,20 @@ const showCategories = (categories)=> {
     for(const cat of categories){
         
         categoriesContainer.innerHTML += `
-        <h1 onclick="loadPlantByCategories(${cat.id})" class="text-[15px] text-gray-400 hover:bg-[#cff0dc] p-3">${cat.category_name}</h1>
+        <h1 onclick="loadPlantByCategories(${cat.id});" id="${cat.id}" class="rounded-sm text-[15px] text-gray-600 hover:bg-[#15a850] p-3 ">${cat.category_name}</h1>
         `
     }
+
+    categoriesContainer.addEventListener('click', (e)=>{
+        const allH1 =categoriesContainer.querySelectorAll('h1')
+        allH1.forEach(h=> h.classList.remove('bg-[#15803D]'))
+        // console.log(allH1)
+
+        if(e.target.localName === 'h1'){
+            e.target.classList.add('bg-[#15803D]')
+        }
+    })
+    
 }
 
 const loadPlantByCategories= (catsid)=>{
@@ -76,7 +87,7 @@ const loadPlantByCategories= (catsid)=>{
     .then((data)=> {
         showPlantByCategories(data.plants)
     })
-
+    
 }
 
 // category:"Fruit Tree"
@@ -90,8 +101,7 @@ const showPlantByCategories = (plantsCat)=>{
     allPlantsContainer.innerHTML= "";
     
     for(const plant of plantsCat){    
-        // console.log(plant)
-
+        
         const description = plant.description
         let shortDescription = "";
         
@@ -99,27 +109,35 @@ const showPlantByCategories = (plantsCat)=>{
             let sd = description[i];
             shortDescription= shortDescription+ sd;
         } 
-
+        
         allPlantsContainer.innerHTML += `
         
         <div class="rounded-3xl shadow-lg overflow-hidden h-[400px]">
-            <img src=${plant.image} alt="" class="h-[190px] w-full object-cover">
-            <div class="p-5 space-y-2">
-                <h2 class="text-[18px] font-semibold">${plant.name}</h2>
-                <p class="text-[12px] font-normal">${shortDescription}...</</p>
-                <div class="flex items-center justify-between">
-                    <p class="text-[15px] text-[#15803D] bg-[#DCFCE7] rounded-3xl p-2">${plant.category}</p>
-                    <p class="text-[15px]">$${plant.price}</p>
-                </div>
-                <button class="btn text-[14px] text-white bg-[#15803D] rounded-3xl w-full">Get Involved</button>
-            </div>
-          </div>
-
+        <img src=${plant.image} alt="" class="h-[190px] w-full object-cover">
+        <div class="p-5 space-y-2">
+        <h2 class="text-[18px] font-semibold">${plant.name}</h2>
+        <p class="text-[12px] font-normal">${shortDescription}...</</p>
+        <div class="flex items-center justify-between">
+        <p class="text-[15px] text-[#15803D] bg-[#DCFCE7] rounded-3xl p-2">${plant.category}</p>
+        <p class="text-[15px]">$${plant.price}</p>
+        </div>
+        <button class="btn text-[14px] text-white bg-[#15803D] rounded-3xl w-full">Get Involved</button>
+        </div>
+        </div>
+        
         `
     }
+
+    
 }
+
 
 
 
 loadCategories();
 allPlantsLoad();
+// const categoriesClickBold = (id)=>{
+// const allCategoriesh1 = document.getElementById("categories-container").querySelectorAll('h1')
+// allCategoriesh1.forEach(h => h.classList.remove('bg-[#15803D] rounded-sm'))
+// const Categoryh1 = document.getElementById(id).classList.add('bg-[#15803D] rounded-sm')
+// }
